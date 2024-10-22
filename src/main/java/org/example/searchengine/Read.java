702ae5bfb919ca489ce970b25efd736a564a0ee2
Eugene kuvaldin <acerkuv@gmail.com>
2024-10-22 18:23:13 +0300

    забота с текстом!

--- src/main/java/org/example/searchengine/Read.java
+++ src/main/java/org/example/searchengine/Read.java
@@ -4,17 +4,18 @@ import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import static java.nio.charset.StandardCharsets.UTF_8;
+
 
 public class Read {
     private String filename;
-    private final HashMap<String, String[]> searchEngine = new HashMap<>();
+    private final HashMap<String, ArrayList<String>> searchEngine = new HashMap<>();
 
     public Read(String filename) {
         this.filename = filename;
     }
-
     public void readFile() throws IOException {
         File file = new File(filename);
         Charset charset = StandardCharsets.US_ASCII;
@@ -33,6 +34,9 @@ public class Read {
         if(lineStrings[0].equals("Add")) {
             String urlSite = lineStrings[lineStrings.length - 1];
             System.out.println(urlSite);
+            String keyWord = lineStrings[2];
+            saveData(urlSite, keyWord);
+
 //            Todo добавить данные в базу
         } else if (lineStrings[0].equals("Search")) {
 //            Todo добавить проверку  на поиск по словам
@@ -41,5 +45,25 @@ public class Read {
 //            Todo добавть удаление из базы
         }
     }
-}
+    public void saveData(String url, String key ){
+        searchEngine.computeIfAbsent(url, k -> new ArrayList<>(Arrays.asList(key)));
+        if(searchEngine.get(url)!=null) {
+            ArrayList<String> keyWords = searchEngine.get(url);
+            boolean isInBase = false;
+//           проверка есть уже кейворд в базе
+            for (String s : keyWords) {
+                if (key.equals(s)) {
+//                   уже есть в базе
+                    isInBase = true;
 
+                }
+
+            }
+            if (!isInBase) {
+                keyWords.add(key);
+                searchEngine.put(url, keyWords);
+            }
+
+        }
+    }
+}
